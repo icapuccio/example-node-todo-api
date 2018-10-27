c55eee980b231822d2845c1f7523e13b14fddefe
Ignacio Capuccio <ignacio.capuccio@widergy.com>
2018-10-27 20:02:44 -0300

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,38 @@
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }, (err, client) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+  const db = client.db('TodoApp');
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('5bd4e3d806ddf0888850f093')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5bd4d8c33fce90856a7e5186')
+  }, {
+      $set: {
+        name: 'José'
+      },
+      $inc: {
+        age: 1
+      }
+    }, {
+      returnOriginal: false
+    }).then((result) => {
+      console.log(result);
+    });
+
+  client.close();
+});
