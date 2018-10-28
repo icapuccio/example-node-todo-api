fbed8e2826de21707ef22da9a9ce5797da09cad4
Ignacio Capuccio <ignacio.capuccio@widergy.com>
2018-10-28 16:35:20 -0300

    Add DELETE /todos/:id route

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,13 @@
+const { ObjectID } = require('mongodb');
+
+const { mongoose } = require('./../server/db/mongoose');
+const { Todo } = require('./../server/models/todo');
+const { User } = require('./../server/models/user');
+
+// Todo.deleteMany({}).then((result) => {
+//   console.log(result);
+// });
+
+Todo.findByIdAndDelete('5bd60cad06ddf08888515929').then((todo) => {
+  console.log(todo);
+});
--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.get('/todos', (req, res) => {
 });
 
 app.get('/todos/:id', (req, res) => {
-  var id = req.params.id
+  var id = req.params.id;
 
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
@@ -46,7 +46,25 @@ app.get('/todos/:id', (req, res) => {
     res.send({todo});
   }, (e) => {
     res.status(400).send();
-  })
+  });
+});
+
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndDelete(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send()
+    }
+
+    res.send({ todo });
+  }, (e) => {
+    res.status(400).send();
+  });
 });
 
 app.listen(port, () => {
