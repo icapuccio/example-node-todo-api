8ca4aab1183c8d20fb3cfe82252981f6d78c6210
Ignacio Capuccio <ignacio.capuccio@widergy.com>
2018-11-04 20:27:45 -0300

    Associate users with todos

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -21,20 +21,28 @@ const users = [
   {
     _id: userTwoId,
     email: 'pepe@example.com',
-    password: 'userTwoPass'
+    password: 'userTwoPass',
+    tokens: [
+      {
+        access: 'auth',
+        token: jwt.sign({ _id: userTwoId, access: 'auth' }, 'abc123').toString()
+      }
+    ]
   }
 ];
 
 const todos = [
   {
     _id: new ObjectID(),
-    text: 'First text todo'
+    text: 'First text todo',
+    _creator: userOneId
   },
   {
     _id: new ObjectID(),
     text: 'Second text todo',
     completed: true,
-    completedAt: 333
+    completedAt: 333,
+    _creator: userTwoId
   }
 ];
 
